cb8a17b2fa69446c14b422e593d0c6dadae176d4
MT1EL <tokochixladze54@gmail.com>
2022-04-02 12:01:53 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-// import { tokeonmicsData } from "./components/tokenomics/tokenomicsData";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AboutUs from "./components/aboutUs";
 import DesignConcept from "./components/DesignConcept";
 import RoadMap from "./components/roadMap/RoadMap";
-// import Table from "./components/tokenomics/table";
 import Tokenomics from "./components/tokenomicsFolder/PieAndTable";
 import Team from "./components/Team/team";
 import Sponsors from "./components/Sponsors";
+import Advisors from "./components/Advisors";
+import Footer from "./components/footer";
 const header = ["pool", "amt Price", "raised", "procent", "tokens"];
 const footer = ["TOTAL", "", "$1.032,480", "100,00%", "100 000 000"];
 function App() {
@@ -24,6 +24,8 @@ function App() {
       <Tokenomics />
       <Team />
       <Sponsors />
+      <Advisors />
+      <Footer />
     </div>
   );
 }
